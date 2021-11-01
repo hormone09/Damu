a1535058688361e43ca6259f57095db0c91a2567
Работа <bau_ke@outlook.com>
2021-11-01 12:04:52 +0600

    Kendo Form

--- FirstTask/Content/js/styles.js
+++ FirstTask/Content/js/styles.js
@@ -2,7 +2,7 @@
 	$('.k-datepicker').width(100);
 	$('.k-textbox').width(150); 
 	$('.k-numerictextbox').width(120);
-	$('.k-combobox').width(170);
+	$('.k-combobox').width(150);
 	$('.k-maskedtextbox').width(120);
-	$('.k-dialog').width(400);
+	$('.k-dialog').width(500);
 });
